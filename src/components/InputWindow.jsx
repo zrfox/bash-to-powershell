29a8ff438976bd64629a175bd136ffdd6293b1a2
Zach <zrfox97@gmail.com>
2026-01-30 17:09:12 -0800

    Add state to InputWindow for getting user input. Will be compared for command matches.

--- src/components/InputWindow.jsx
+++ src/components/InputWindow.jsx
@@ -5,8 +5,19 @@ import { useState, useId } from 'react'
 function InputWindow({ onRemove, languageName }){
 // idk if i need this here or in homepage. 
 
+const [text, setText] = useState();
+
 const inputTextAreaId = useId();
 
+
+
+function setTextHandler(inputText) {
+    setText(inputText);
+}
+
+function checkMatch() {
+    
+}
     return (
         <>
         <div className='input-window'>
@@ -16,6 +27,7 @@ const inputTextAreaId = useId();
             <textarea
                 id={inputTextAreaId}
                 name='inputTextArea'
+                onChange={(e) => setText(e.target.value)}
             />
             
         </div>
